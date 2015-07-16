b81f9736569b2a502ed52f6635ca7b80d22f1f5a
jonathanharijanto <harijanj@onid.oregonstate.edu>
2015-07-15 22:56:00 -0700

    edit a little more

--- javascript.js
+++ javascript.js
@@ -1,1 +1,40 @@
 //javascript functions
+//javascript functions
+
+var _url = "http://crossorigin.me/https://api.github.com/gists"
+
+window.onload = function() {
+	var submitButton = document.getElementById("tfq");
+	if (submitButton.addEventListener) {
+		submitButton.addEventListener("click", function(){
+			if(submitButton.value == "Enter keywords...") {
+				submitButton.value = "";
+			}
+		});
+	}
+}
+
+function loadXMLDoc() {
+	var xmlhttp;
+	if (window.XMLHttpRequest) {
+		xmlhttp = new XMLHttpRequest();
+	}
+	else {
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+}
+
+function makeAjaxCall(url) {
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send(null);
+
+	xmlhttp.onreadystatechange = function() {
+		if(xmlhttp.readyState == 4) {
+			if (xmlhttp.status == 200) {
+			//responseText
+			}
+		} else {
+			alert("Error!");
+		}
+	}
+}
