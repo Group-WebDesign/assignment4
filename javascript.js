13a5ff8f4f1db0c916ffe4bea5b6a0e6a08020e8
jonathanharijanto <harijanj@onid.oregonstate.edu>
2015-07-16 16:58:04 -0700

    group meeting 1

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 //javascript functions
-//javascript functions
 
-var _url = "http://crossorigin.me/https://api.github.com/gists"
+var _url = "https://api.github.com/gists";
+var gistsDom =  document.getElementById("gists");
 
 window.onload = function() {
 	var submitButton = document.getElementById("tfq");
@@ -12,29 +12,68 @@ window.onload = function() {
 			}
 		});
 	}
+
+	var trialGist = GET /gists/public;
 }
 
 function loadXMLDoc() {
-	var xmlhttp;
+	var request;
 	if (window.XMLHttpRequest) {
-		xmlhttp = new XMLHttpRequest();
+		request = new XMLHttpRequest();
 	}
 	else {
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		request = new ActiveXObject("Microsoft.XMLHTTP");
 	}
 }
 
-function makeAjaxCall(url) {
-	xmlhttp.open("GET", url, true);
-	xmlhttp.send(null);
+function gists (description, id, html_url) {
+	this.description = description;
+	this.id = id;
+	this.url = html_url;
+	this.convertToHtml = function () {
+		var holder = document.createElement("div");
+		holder.className = "gistsHolder";
+		var url = document.createElement("div");
+		var description = document.createElement("div");
+		var id = document.createElement("div");
 
-	xmlhttp.onreadystatechange = function() {
-		if(xmlhttp.readyState == 4) {
-			if (xmlhttp.status == 200) {
-			//responseText
+		url.className = "gistsUrl";
+		id.className = "gistsId";
+		description.className =	"gistsDescription";
+
+		description.innerHTML = this.description;
+		
+		id.innerHTML = this.id;
+
+		url.innerHTML = this.url;
+
+		holder.appendChild(html_url);
+		holder.appendChild(id);
+		holder.appendChild(description);
+
+		gistsDom.appendChild(holder);
+	}
+
+function makeAjaxCall(url) {
+	request.open("GET", url, true);
+	request.send(null);
+
+	request.onreadystatechange = function() {
+		if(request.readyState == 4) {
+			if (request.status == 200) {
+				var serverResponse = JSON.parse(request.responseText);
+				var gists = serverResponse.data.gists;
+				var gistsArray = [];
+				for (var i = 0; i < gists.length; i ++) {
+					var g = new gists(gists[i].description, gists[i].id, gists[i].url);
+					gistsArray.push(g);
+					g.convertToHtml();
+				}
 			}
 		} else {
 			alert("Error!");
 		}
 	}
 }
+
+makeAjaxCall(_url);
